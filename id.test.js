5c547b42df6ed3f8db51821f7f3602f9d73ea41b
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-06-04 09:16:38 +1200

    write out list of tests to work through

--- id.test.js
+++ id.test.js
@@ -4,3 +4,18 @@ describe ('Set up tests', () => {  //describe will break your test results into 
     expect(1).toBe(1);
   });
 })
+
+//TESTS
+// 1. Create a random number
+// 2. The number is an integer.
+// 3. The number created is within a specified range.
+// 4. The number is unique.
+
+describe ('create a unique ID number', () => {
+  test('creates a random number', (){
+
+  })
+  test.todo('checks the number is an integer')
+  test.todo('checks the number created is within a specific range')
+  test.todo('checks the number is unique')
+})
